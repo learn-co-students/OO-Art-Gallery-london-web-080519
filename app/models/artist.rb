7f8b6965af073cf8a947f6a78a9bf7fea82426a5
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-08-13 13:22:43 +0100

    Artist methods

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,40 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
-  def initialize(name, years_experience)
+  def initialize(name:, years_experience:)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map {|painting| painting.gallery.name }
+  end
+
+  def cities
+    paintings.map {|painting| painting.gallery.city}
+  end
+
+  def self.total_experience
+    self.all.map{|artist| artist.years_experience}.reduce(:+)
+  end
+
+  def self.most_prolific
+    #highest amount of paintings PER year of experience
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title: title, price: price, artist: self, gallery: gallery)
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,24 @@
 require_relative '../config/environment.rb'
 
+# Artist.new(name:, years_experience:)
+davinci = Artist.new(name: "Da Vinci", years_experience: 10)
+erin = Artist.new(name: "Erin", years_experience: 1)
+ben = Artist.new(name: "Ben", years_experience: 5)
 
+
+# Gallery.new(name:, city:)
+louvre = Gallery.new(name: "The Louvre", city: "Paris")
+newyorkbluechip = Gallery.new(name: "Blue Chip Gallery", city: "New York City")
+
+
+# Painting.new(title:, price:, artist:, gallery:)
+monalisa = Painting.new(title: "Mona Lisa", price: 10000, artist:davinci, gallery: louvre)
+erinspainting = Painting.new(title: "a better painting", price: 100000, artist:erin, gallery: newyorkbluechip)
+benspainting = Painting.new(title: "an even better painting", price: 200, artist:ben, gallery: newyorkbluechip)
 
 
 binding.pry
 
 puts "Bob Ross rules."
+#Yes he does :)
+
